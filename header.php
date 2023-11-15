e3e383b1e43527a325251e748f26aee599ba2c5e
Duongbro <nguyenduongthai000@gmail.com>
2023-11-15 15:07:11 +0700

    N4-10_[HTML] - Chỉnh sửa header

---
+++ header.php
@@ -0,0 +1,44 @@
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="?page=home">Trang chủ</a></li>
+                        <li class="scroll-to-section"><a href="?page=about">Giới thiệu</a></li>
+                        <li class="scroll-to-section"><a href="?page=contact">Liên hệ</a></li>
+                        <li class="scroll-to-section"><a href="?page=cart">Giỏ hàng</a></li>
+                        <li class="submenu">
+                            <a href="?page=product">Sản phẩm</a>
+                            <ul>
+                                <li><a href="">Nam</a></li>
+                                <li><a href="">Nữ</a></li>
+                                <li><a href="">Thể thao</a></li>
+                            </ul>
+                        </li>
+                        <!-- <li class="submenu">
+                            <a href="javascript:;">Features</a>
+                            <ul>
+                                <li><a href="#">Features Page 1</a></li>
+                                <li><a href="#">Features Page 2</a></li>
+                                <li><a href="#">Features Page 3</a></li>
+                                <li><a rel="nofollow" href="https://templatemo.com/page/4" target="_blank">Template Page 4</a></li>
+                            </ul>
+                        </li> -->
+                        <li class="scroll-to-section"><a href="?page=login">Đăng nhập/Đăng ký</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
