dc28115330141150c7a769982e9886d8347003ff
Duongbro <nguyenduongthai000@gmail.com>
2023-11-15 15:14:30 +0700

    N4-11 [HTML] - Chỉnh sửa footer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<footer style="margin-top: 0px; float: left; width:100%;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="first-item">
+                    <div class="logo">
+                        <img src="assets/images/white-logo.png" alt="hexashop ecommerce templatemo">
+                    </div>
+                    <ul>
+                        <li><a href="#">14B1 Trương Vĩnh Nguyên, Thường Thạnh, Cái Răng, Cần Thơ, Việt Nam</a></li>
+                        <li><a href="#">kimwatch@gmail.com</a></li>
+                        <li><a href="#">098-237-4851</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <h4>Chính Sách</h4>
+                <ul>
+                    <li><a href="#">Chính sách bảo mật</a></li>
+                    <li><a href="#">Chính sách Bảo hành & Đổi trả</a></li>
+                    <li><a href="#">Chính sách Thành viên</a></li>
+                    <li><a href="#">Chính sách giao hàng hỏa tốc</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <h4>Liên kết hữu ích</h4>
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Hỗ trợ</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3">
+                <h4>Hỗ trợ khách hàng</h4>
+                <ul>
+                    <li><a href="#">Hỗ trợ</a></li>
+                    <li><a href="#">FAQ's</a></li>
+                    <li><a href="#">Giao hàng</a></li>
+                    <li><a href="#">Thông tin khách hàng</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-12">
+                <div class="under-footer">
+                    <p>Copyright © 2022 HexaShop Co., Ltd. All Rights Reserved.
+
+                        <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
+                    </p>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
