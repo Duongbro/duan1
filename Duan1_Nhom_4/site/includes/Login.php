df536e422ee1c64cb930ee77d70fb64346bc72dc
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:44:50 +0700

    N3-17

--- Duan1_Nhom_4/site/includes/Login.php
+++ Duan1_Nhom_4/site/includes/Login.php
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dangnhap.css">
+</head>
+<body>
+<div class="parent clearfix">
+    <div class="bg-illustration">
+      <img src="" alt="logo">
 
+      <div class="burger-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+    
+    <div class="login">
+      <div class="container">
+        <h1>Bạn có thể đăng <br /> nhập tại đây</h1>
+    
+        <div class="login-form">
+          <form action="">
+            <input type="username" placeholder="Tên đăng nhập">
+            <input type="password" placeholder="Mật khẩu">
+            
+
+            <div class="remember-form">
+              <input type="checkbox">
+              <span>Ghi nhớ đăng nhập</span>
+            </div>
+            <div class="forget-pass">
+              <a href="#">Quên mật khẩu ?</a>
+            </div>
+
+            <button type="submit">Đăng nhập</button>
+
+          </form>
+        </div>
+    
+      </div>
+      </div>
+  </div>
+    
+</body>
+</html>
