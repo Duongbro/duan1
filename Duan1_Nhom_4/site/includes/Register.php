c2764f6bde593e15ab7797557c6b879d99b550fb
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:46:55 +0700

    N3-19

---
+++ Duan1_Nhom_4/site/includes/Register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dangky.css">
+</head>
+<body>
+<div class="parent clearfix">
+    <div class="bg-illustration">
+      <img src="" alt="logo">
+
+      <div class="burger-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+    
+    <div class="login">
+      <div class="container">
+        <h1>Bạn có thể đăng <br /> ký tại đây</h1>
+    
+        <div class="login-form">
+          <form action="">
+            <input type="username" placeholder="Tên đăng nhập">
+            <input type="password" placeholder="Mật khẩu">
+            <input type="email" placeholder="Email">
+            
+
+            <div class="remember-form">
+              <input type="checkbox">
+              <span>Ghi nhớ</span>
+            </div>
+            <div class="forget-pass">
+              <a href="#">Đăng nhập</a>
+            </div>
+
+            <button type="submit">Đăng ký</button>
+
+          </form>
+        </div>
+    
+      </div>
+      </div>
+  </div>
+    
+</body>
+</html>
