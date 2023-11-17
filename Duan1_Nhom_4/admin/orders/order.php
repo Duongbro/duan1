8c1e4c52600c2032b231261b60f90da5878110f7
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:34:17 +0700

    N3-42

--- Duan1_Nhom_4/admin/orders/order.php
+++ Duan1_Nhom_4/admin/orders/order.php
@@ -4,7 +4,7 @@ class oders
     
     var $oders_id = null;
     var $fullname = null;
-    var $ema = null;
+    var $email = null;
     var $phone = null;
     var $address= null;
     var $note = null;
