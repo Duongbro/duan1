8a32eea3b2d47d760813a9097d483232bc35cb52
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:38:23 +0700

    N3-39

--- Duan1_Nhom_4/admin/category/category.php
+++ Duan1_Nhom_4/admin/category/category.php
@@ -9,7 +9,7 @@ class categories
     public function getlist()
     {
         $db = new connect();
-        $query = "SELECT*FROM category";
+        $query = "SELECT*FROM categories";
         $result = $db->pdo_query($query);
         return $result;
     }
@@ -17,14 +17,14 @@ class categories
     public function getById($category_id)
     {
         $db = new connect();
-        $query = "SELECT*FROM category WHERE id=" .$category_id;
+        $query = "SELECT*FROM categories WHERE id=" .$category_id;
         $result = $db->pdo_query_one($query);
         return $result;
     }
     // viết một hàm insert dữ liệu, thêm mới dữ liệu
     public function add($name){
         $db = new connect();
-        $query = "INSERT INTO `category`(`name`) VALUES ('$name')";
+        $query = "INSERT INTO `categories`(`name`) VALUES ('$name')";
         $result = $db->pdo_execute($query);
         return $result;
     }
@@ -33,7 +33,7 @@ class categories
     public function update( $category_id ,$name)
     {
         $db = new connect();
-        $query = "UPDATE `category` SET `name`='$name' WHERE id =  $category_id";
+        $query = "UPDATE `categories` SET `name`='$name' WHERE id =  $category_id";
         $result = $db->pdo_execute($query);
         return $result;
     }
@@ -41,7 +41,7 @@ class categories
     public function delete($category_id)
     {
         $db = new connect();
-        $query = "DELETE  FROM category WHERE id='$category_id'";
+        $query = "DELETE  FROM categories WHERE id='$category_id'";
         $result = $db->pdo_execute($query);
         return $result;
     }
