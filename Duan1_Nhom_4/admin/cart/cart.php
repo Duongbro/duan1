35a25dcc72506e03ce5b5d302eb80ea16e265f68
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:12:55 +0700

    N3-43

--- Duan1_Nhom_4/admin/cart/cart.php
+++ Duan1_Nhom_4/admin/cart/cart.php
@@ -2,10 +2,10 @@
 class carts
 {
     
-    var $id_carts = null;
-    var $id_product = null;
+    var $carts_id = null;
+    var $product_id = null;
     var $password = null;
-    var $id_user = null;
+    var $user_id = null;
     var $quantity= null;
     var $status = null;
    
@@ -19,34 +19,34 @@ class carts
         return $result;
     }
 
-    public function getById($id_carts)
+    public function getById( $carts_id)
     {
         $db = new connect();
-        $query = "SELECT*FROM carts WHERE id=" .$id_carts;
+        $query = "SELECT*FROM carts WHERE id=" . $carts_id;
         $result = $db->pdo_query_one($query);
         return $result;
     }
     // viết một hàm insert dữ liệu, thêm mới dữ liệu
-    public function add($id_product ,$password, $id_user,$quantity, $status){
+    public function add($product_id ,$password, $id_user,$quantity, $status){
         $db = new connect();
-        $query = "INSERT INTO `carts`(`id_product`,`password`,`id_user`,`quantity`,`status`) VALUES ('$id_product','$id_user','$email','$quantity','$status')";
+        $query = "INSERT INTO `carts`(`$product_id`,`password`,`id_user`,`quantity`,`status`) VALUES ('$product_id','$id_user','$email','$quantity','$status')";
         $result = $db->pdo_execute($query);
         return $result;
     }
 
     // hàm cập nhật dữ liệu
-    public function update($id_carts,$id_product ,$password, $id_user,$quantity, $status )
+    public function update( $carts_id,$product_id ,$password, $id_user,$quantity, $status )
     {
         $db = new connect();
-        $query = "UPDATE `carts` SET `$id_product`='$id_product', `password` = '$password',`id_user`='$id_user', `quantity`='$quantity', `status`='$status' WHERE id = $id_carts";
+        $query = "UPDATE `carts` SET `$product_id`='$product_id', `password` = '$password',`id_user`='$id_user', `quantity`='$quantity', `status`='$status' WHERE id =  $carts_id";
         $result = $db->pdo_execute($query);
         return $result;
     }
     
-    public function delete($id_carts)
+    public function delete( $carts_id)
     {
         $db = new connect();
-        $query = "DELETE  FROM carts WHERE id='$id_carts'";
+        $query = "DELETE  FROM carts WHERE id=' $carts_id'";
         $result = $db->pdo_execute($query);
         return $result;
     }
