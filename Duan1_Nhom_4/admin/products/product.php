cd1ffaffdc6a68ec47a936c09abb99dc25ce327f
zemomv <150119896+zemomv@users.noreply.github.com>
2023-11-17 13:29:49 +0700

    N3-40

--- Duan1_Nhom_4/admin/products/product.php
+++ Duan1_Nhom_4/admin/products/product.php
@@ -2,13 +2,14 @@
 class products
 {
     
-    var $id_product = null;
+    var $product_id = null;
     var $name = null;
     var $sale_price = null;
     var $descibe = null;
     var $img= null;
-    var $id_category = null;
+    var $category_id = null;
     var $quantity = null;
+    var $price= null;
     
     
 
@@ -23,31 +24,31 @@ class products
     public function getById($id_product)
     {
         $db = new connect();
-        $query = "SELECT*FROM products WHERE id=" .$id_product;
+        $query = "SELECT*FROM products WHERE id=" .$product_id;
         $result = $db->pdo_query_one($query);
         return $result;
     }
     // viết một hàm insert dữ liệu, thêm mới dữ liệu
-    public function add($name,$sale_price,$descibe,$img, $id_category, $quantity){
+    public function add($name,$sale_price,$descibe,$img, $category_id,$price, $quantity){
         $db = new connect();
-        $query = "INSERT INTO `products`(`name`,`sale_price`,`descibe`,`img`,`id_category`,`quantity`) VALUES ('$name','$sale_price','$descibe','$img','$id_category','$quantity')";
+        $query = "INSERT INTO `products`(`name`,`sale_price`,`descibe`,`img`,`category_id`,`quantity`) VALUES ('$name','$sale_price','$descibe','$img','$category_id','$quantity')";
         $result = $db->pdo_execute($query);
         return $result;
     }
 
     // hàm cập nhật dữ liệu
-    public function update($id_product,$name,$sale_price,$descibe, $img, $id_category, $quantity)
+    public function update($product_id,$name,$sale_price,$descibe, $img, $category_id,$price, $quantity)
     {
         $db = new connect();
-        $query = "UPDATE `products` SET `name`='$name', `sale_price` = '$sale_price',`descibe`='$descibe', `img`='$img', `id_category`='$id_category', `quantity`='$quantity' WHERE id = $id_product";
+        $query = "UPDATE `products` SET `name`='$name', `sale_price` = '$sale_price',`descibe`='$descibe', `img`='$img', `id_category`='$category_id',`price`='price', `quantity`='$quantity' WHERE id = $id_product";
         $result = $db->pdo_execute($query);
         return $result;
     }
     
-    public function delete($id_product)
+    public function delete($product_id)
     {
         $db = new connect();
-        $query = "DELETE  FROM products WHERE id='$id_product'";
+        $query = "DELETE  FROM products WHERE id='$product_id'";
         $result = $db->pdo_execute($query);
         return $result;
     }
