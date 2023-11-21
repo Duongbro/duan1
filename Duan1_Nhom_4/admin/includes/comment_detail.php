6d696eace64f4e2151d3855e055fadf67ef52543
Duongbro <nguyenduongthai000@gmail.com>
2023-11-21 15:48:44 +0700

    [N3-73]- [HTML] - Chỉnh sửa trang chi tiết bình luận (admin)

---
+++ Duan1_Nhom_4/admin/includes/comment_detail.php
@@ -0,0 +1,58 @@
+<div class="container-scroller">
+    <?php include "header.php"; ?>
+    <!-- partial -->
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="page-header">
+                <h3 class="page-title">
+                    <span class="page-title-icon bg-gradient-primary text-white me-2">
+                        <i class="mdi mdi-comment-processing"></i>
+                    </span> Quản lý bình luận
+                </h3>
+                <nav aria-label="breadcrumb">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <span></span>Overview <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="row">
+                <div class="col-12 grid-margin">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Chi tiết bình luận</h4>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th style="width:300px;"> Nội dung </th>
+                                            <th style="width:150px;"> Người bình luận </th>
+                                            <th style="width:150px;"> Ngày bình luận </th>
+                                            <th style="width:150px;"> </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Cái này đẹp quá</td>
+                                            <td>Nguyễn Thái Dương</td>
+                                            <td>18-11-2023</td>
+                                            <td><button type="button" class="btn btn-gradient-primary btn-sm">Xóa</button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <?php include "footer.php"; ?>
+        <!-- partial -->
+    </div>
+    <!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+</div>
