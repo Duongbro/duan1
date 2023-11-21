39923983ba5570e5e759fc4e0877e34e05fd84ef
Duongbro <nguyenduongthai000@gmail.com>
2023-11-21 20:40:23 +0700

    [N3-44]- [HTML] - Chỉnh sửa trang sản phẩm của user

---
+++ Duan1_Nhom_4/site/includes/product.php
@@ -0,0 +1,189 @@
+<?php include "header.php" ?>
+<section class="section" id="products" style="margin-top: 50px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading">
+                    <h2>Tất cả sản phẩm</h2>
+                    <span>
+                        <hr>
+                    </span>
+                    <!-- <form class="form-search" action="?page=search" method="POST">
+                        <input placeholder="Tìm kiếm ... " required="required" class="input-search" type="text" name="txt_search">
+                        <button type="submit" class='search'>Tìm</button>
+                    </form> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="?page=detail"><i class="fa fa-eye"></i></a></li>
+                                <li><a href=""><i class="fa fa-star"></i></a></li>
+                                <li><a href="?page=cart"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/dongho1.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Casio 43.5mm Nam MTP-1374D-1AVDF</h4>
+                        <span style="color: red;">1.740.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="?page=detail"><i class="fa fa-eye"></i></a></li>
+                                <li><a href=""><i class="fa fa-star"></i></a></li>
+                                <li><a href="?page=cart"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/dongho2.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Olym Pianus 42mm Nam OP990-45ADGS-GL-X</h4>
+                        <span style="color:red;">6.550.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="?page=detail"><i class="fa fa-eye"></i></a></li>
+                                <li><a href=""><i class="fa fa-star"></i></a></li>
+                                <li><a href="?page=cart"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/dongho3.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Casio 42mm Nam MTP-1375L-1AVDF</h4>
+                        <span style="color:red;">1.670.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="?page=detail"><i class="fa fa-eye"></i></a></li>
+                                <li><a href=""><i class="fa fa-star"></i></a></li>
+                                <li><a href="?page=cart"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/dongho4.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Casio 41mm Nam MTP-1384L-7AVDF</h4>
+                        <span style="color:red;">1.740.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-star"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/donghonu1.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Casio 22mm Nữ LTP-1274D-7ADF</h4>
+                        <span style="color:red;">1.000.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-star"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/donghonu2.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Orient 31mm Nữ FNR1Q001W0 </h4>
+                        <span style="color:red;">4.420.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-star"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/donghonu3.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ SRWatch 30mm Nữ SL1605.1102TE</h4>
+                        <span style="color:red;">1.320.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="item">
+                    <div class="thumb">
+                        <div class="hover-content">
+                            <ul>
+                                <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-star"></i></a></li>
+                                <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                            </ul>
+                        </div>
+                        <img src="assets/images/donghonu4.webp" alt="">
+                    </div>
+                    <div class="down-content">
+                        <h4>Đồng Hồ Tissot 35mm Nữ T050.207.37.017.05</h4>
+                        <span style="color:red;">10.600.000₫</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="pagination">
+                    <ul>
+                        <li class="active">
+                            <a href="#">1</a>
+                        </li>
+                        <li>
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#">4</a>
+                        </li>
+                        <li>
+                            <a href="#">></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include "footer.php" ?>
