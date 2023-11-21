a4b6af9b4233bf87196a640ebd72bb50bf3dfaf5
Duongbro <nguyenduongthai000@gmail.com>
2023-11-21 14:51:59 +0700

    [N3-17]-[HTML] - Chỉnh sửa trang đăng nhập [Cập nhật mới]

--- Duan1_Nhom_4/site/includes/Login.php
+++ Duan1_Nhom_4/site/includes/Login.php
@@ -1,50 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="dangnhap.css">
-</head>
-<body>
-<div class="parent clearfix">
-    <div class="bg-illustration">
-      <img src="" alt="logo">
 
-      <div class="burger-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
+<div class="container" id="container">
+    <div class="form-container sign-up-container">
+        <form action="#" class="form">
+            <h1 class="h1">Đăng Ký</h1>
+            <!-- <div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span> -->
+            <input type="text" placeholder="Nhập tên đăng nhập" class="input"/>
+            <input type="email" placeholder="Nhập email" class="input"/>
+            <input type="password" placeholder="Nhập mật khẩu" class="input"/>
+            <button>Đăng Ký</button>
+        </form>
     </div>
-    
-    <div class="login">
-      <div class="container">
-        <h1>Bạn có thể đăng <br /> nhập tại đây</h1>
-    
-        <div class="login-form">
-          <form action="">
-            <input type="username" placeholder="Tên đăng nhập">
-            <input type="password" placeholder="Mật khẩu">
-            
-
-            <div class="remember-form">
-              <input type="checkbox">
-              <span>Ghi nhớ đăng nhập</span>
+    <div class="form-container sign-in-container">
+        <form action="#" class="form">
+            <h1 class="h1">Đăng Nhập</h1>
+            <div class="social-container">
+                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
             </div>
-            <div class="forget-pass">
-              <a href="#">Quên mật khẩu ?</a>
+            <span>hoặc sử dụng tài khoản của bạn</span>
+            <input type="text" placeholder="Tên đăng nhập" class="input"/>
+            <input type="password" placeholder="Mật khẩu" class="input"/>
+            <a href="#">Quên mật khẩu?</a>
+            <button>Đăng nhập</button>
+        </form>
+    </div>
+    <div class="overlay-container">
+        <div class="overlay">
+            <div class="overlay-panel overlay-left">
+                <h1 class="h1">Chào mừng trở lại!</h1>
+                <p style="color:white;font-weight: 500;" class="p">Để duy trì kết nối với chúng tôi vui lòng đăng nhập bằng thông tin cá nhân của bạn</p>
+                <button class="ghost" id="signIn">Đăng Nhập</button>
             </div>
-
-            <button type="submit">Đăng nhập</button>
-
-          </form>
+            <div class="overlay-panel overlay-right">
+                <h1 class="h1">Chào bạn!</h1>
+                <p style="color:white;font-weight: 500;" class="p">Nhập thông tin cá nhân của bạn và bắt đầu hành trình với chúng tôi</p>
+                <button class="ghost" id="signUp">Đăng Ký</button>
+            </div>
         </div>
-    
-      </div>
-      </div>
-  </div>
-    
-</body>
-</html>
+    </div>
+</div>
