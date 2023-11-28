0562d8b1161a717226d36c9745c19689c6e58150
Duongbro <nguyenduongthai000@gmail.com>
2023-11-28 15:21:02 +0700

    [N3-63]-[[CORE] - Viết hàm của bảng comments][Update - 28/11/2023]

--- Duan1_Nhom_4/admin/comment/comment.php
+++ Duan1_Nhom_4/admin/comment/comment.php
@@ -1,64 +1,108 @@
 <?php
-class comments
+
+$db = '';
+
+class comment
 {
-    
-    var $comments_id = null;
+    var $comment_id = null;
     var $product_id = null;
     var $user_id = null;
-    var $date= null;
+    var $date = null;
     var $content = null;
-    
-  
 
-    public function getlist()
+
+
+    public function getList()
     {
         $db = new connect();
-        $query = "SELECT*FROM comments";
+        $query = 'SELECT * FROM commnet';
         $result = $db->pdo_query($query);
         return $result;
     }
 
-    public function getById($comments_id)
+    public function binhluan_select_by_id_product($id_product)
     {
         $db = new connect();
-        $query = "SELECT*FROM comments WHERE id=" . $comments_id;
-        $result = $db->pdo_query_one($query);
+        $query = 'SELECT cmt.*pd.nameProduct FROM comment cmt JOIN pd id_product ON cmt.id_product = pd.id_product WHERE cmt.id_product =' . $id_product;
+        $result = $db->pdo_query_one($query, $id_product);
         return $result;
     }
-    // viết một hàm insert dữ liệu, thêm mới dữ liệu
-    public function add($product_id,$user_id, $date, $content){
+    public function add_comment($id_product, $content_cmt)
+    {
         $db = new connect();
-        $query = "INSERT INTO `commenst`(`product_id`,`user_id`,`date`,`conten`) VALUES ('$product_id','$user_id','$date','$content')";
+        $query = "INSERT INTO comment (id_product, content_cmt) VALUES ('$id_product', '$content_cmt')";
         $result = $db->pdo_execute($query);
         return $result;
     }
+    public function getComMentDescLimit () {
+        $db = new connect () ;
+        $sql = "SELECT * FROM comment ORDER BY comment.id_comment DESC LIMIT 1" ;
+        $result = $db -> pdo_query ($sql) ;
+        return $result ; 
+    }
 
-    // hàm cập nhật dữ liệu
-    public function update($product_id ,$user_id, $date, $content)
-    {
-        $db = new connect();
-        $query = "UPDATE `comments` SET `product_id`='$product_id', `user_id` = '$user_id',`date`='$date',  `content`='$content' WHERE id =  $comments_id";
-        $result = $db->pdo_execute($query);
+    public function add_detail ($id_user , $id_comment) {
+        $db = new connect () ;
+        $sql = "INSERT INTO detail_cmt (id_user , id_comment) VALUES ('$id_user' , '$id_comment')";
+        $result = $db -> pdo_execute ($sql);
+        return $result; 
+    }
+
+    public function getComment ($id_product) {
+        $db = new connect ();
+        $sql = "SELECT comment.content_cmt AS content , detail_cmt.id_user AS IdUser, comment.date_cmt AS datecmt,  user.nameUser as ten, user.imgUser as avatar ,comment.id_comment
+        FROM comment , detail_cmt, user 
+        WHERE comment.id_product = '$id_product'
+        AND detail_cmt.id_comment = comment.id_comment
+        AND detail_cmt.id_user = user.id_user";
+        $result = $db -> pdo_query ($sql);
+        return $result;
+    }
+
+    public function getProduct ($id_product) {
+        $db = new connect ();
+        $sql = "SELECT COUNT(*) AS count 
+        FROM comment, detail_cmt 
+        WHERE comment.id_comment = detail_cmt.id_cmt_detail 
+        AND comment.id_product = '$id_product'";
+        $result = $db -> pdo_query ($sql);
+        return $result;
+    }
+
+    public function getComment_Admin () {
+        $db = new connect ();
+        $sql = "SELECT products.name AS name, COUNT(*) AS count, MIN(date) AS old, MAX(date) AS new, products.product_id
+        FROM comments, products
+        WHERE comments.product_id = products.product_id
+        GROUP BY products.product_id";
+        $result = $db -> pdo_query ($sql);
+        return $result;
+    }
+
+    public function getComment_Detail_Admin ($product_id) {
+        $db = new connect ();
+        $sql = "SELECT comments.content AS content, comments.date AS date, users.name AS user_cmt, products.product_id, comments.comment_id
+        FROM  comments, users, products
+        WHERE comments.product_id = products.product_id
+        AND   comments.user_id = users.user_id
+        AND   comments.product_id =  '$product_id'";
+        $result = $db -> pdo_query ($sql);
+        return $result;
+    }
+
+    public function delete_comment ($comment_id) {
+        $db = new connect ();
+        $sql = "DELETE FROM comments WHERE comment_id = ?";
+        $result = $db -> pdo_execute($sql, $comment_id);
         return $result;
     }
     
-    public function delete($comments_id)
+    public function getCount()
     {
         $db = new connect();
-        $query = "DELETE  FROM comments WHERE id=' $comments_id'";
-        $result = $db->pdo_execute($query);
+        $select = "SELECT count(*) AS soluong FROM comment";
+        $result = $db->pdo_query($select);
         return $result;
     }
-
-}    
-?>    
-
     
-    
-    
-    
-    
-
-    
-
-    
+}
